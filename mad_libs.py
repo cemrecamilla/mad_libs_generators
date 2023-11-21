efbd10501a5addd95b819be515c8d35957ff1fce
cemrecamilla <144738429+cemrecamilla@users.noreply.github.com>
2023-11-21 23:35:18 +0300

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,19 @@
+#mad libs generator
+
+noun   = input("choose a noun: ")
+p_noun = input("choose a plural noun: ")
+noun2  = input("choose a noun: ")
+place  = input("name a place: ")
+adjactive = input("choose an adjective (Describing word):")
+noun3  = input ("choose a noun: ")
+
+#print a story from the user input 
+
+print("----------------------")
+print("be kind to your", noun, "- footed", p_noun)
+print("For a duck may be somebody\"s",noun2, ",")
+print("be kind to your", p_noun, "in", place)
+print("where the weather is always", adjactive, ". \n")
+print("you may think that is this the", noun3, ",")
+print("well it is.")
+print("--------------------")
